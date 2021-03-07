c200effcff57ba493709da7bd1190be722673a03
Andy Truong <att35663@uga.edu>
2021-03-07 18:23:06 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const admin = require("firebase-admin");
+const app = express();
+
+app.use(express.json());
+app.engine("html", require("ejs").renderFile);
+
+const serviceAccount = require("./key.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('This is the homepage');
+})
+
+app.get('/signup', (req, res) => {
+    res.render("login.ejs", {
+        testData: "This is some test data"
+    });
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
